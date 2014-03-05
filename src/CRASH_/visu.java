62836646bbc4cf16bd04867452c87335f3b15205
ebardiere <ebardiere@rksw1306w343>
2014-03-05 10:35:00 +0100

    Modification du .project (EB)

--- src/CRASH_/visu.java
+++ src/CRASH_/visu.java
@@ -1,10 +1,10 @@
-package joo;
+package CRASH_;
 
 public class visu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("1234");
+		System.out.println("1234");
 	}
 
 }
