3a38e164b8fa578eff5cd79ad6e8ae2f537b0c92
VCARON <VCARON@RKS1306W319.ensg1.ensg.eu>
2014-03-10 15:32:13 +0100

    update

--- src/gendarmerie/pageAccueil.java
+++ src/gendarmerie/pageAccueil.java
@@ -20,11 +20,6 @@ public class pageAccueil extends JPanel{
 */
 static JTextField filename = new JTextField();
 
-/**
-* utilisée dans la classe charger pour stocker le nom du chemin dans lequel
-* se trouve le nom du fichier que l'on veut charger
-*/
-//static JTextField dir = new JTextField();
 
 /**
 * Constructeur de la page d'accueil. Il génère une image de fond sur laquelle
@@ -35,53 +30,124 @@ pageAccueil() {
 
 
 /**
-* crée une étiquette permettant de mettre une image de fond à la page d'acccueil
+* Instancie la classe panneauImage permettant de mettre une image de fond à la page d'acccueil
 */
 panneauImage panneau= new panneauImage();
 
 /**
-* Affecte un gestionnaire de présentation GridBagLayout à cette étiquettequi va permettre de positionner correctement
-* les boutons et les messages
+* Affecte un gestionnaire de présentation GridBagLayout à panneau qui va permettre de positionner correctement
+* les boutons et les messages sur le panneau
 */
 GridBagLayout disposition1 = new GridBagLayout();
 panneau.setLayout(disposition1);
 
 /**
-* Crée le message de bienvenue et le positionne dans la zone nord de la fenêtre
+* Crée le message de bienvenue et le positionne dans la zone nord centrée de la fenêtre. Pour centrer le message, on crée
+* 2 labels vides qui encadrent le label contenant le message de bienvenue 
 */
 
-// Crée une instance de GridBagConstraints pour le positionnement du message de bienvenue
+//Crée une instance de GridBagConstraints pour le positionnement du label vide1
+GridBagConstraints contr0 = new GridBagConstraints();
+
+//Crée les contraintes de positionnement d'un champ
+
+//coordonnée x dans le quadrillage
+contr0.gridx = 0;
+//coordonnée y dans le quadrillage
+contr0.gridy = 0;
+//hauteur standard
+contr0.gridheight = 1;
+//largeur standard
+contr0.gridwidth = 1;
+//remplit tout l'espace dans la cellule
+contr0.fill = GridBagConstraints.BOTH;
+//proportion d'espace horizontal occupée par ce composant
+contr0.weightx = 1;
+//proportion d'espace vertical occupée par ce composant
+contr0.weighty = 0.1;
+//position du composant dans la cellule
+contr0.anchor = GridBagConstraints.CENTER;
+
+/**
+* Crée le label vide1
+*/
+JLabel vide1= new JLabel();
+
+
+//Affecte les contraintes au label vide1
+disposition1.setConstraints(vide1, contr0);
+
+//Ajoute le label vide1 au panneau
+panneau.add(vide1);
+
+//Crée une instance de GridBagConstraints pour le positionnement du label vide2
 GridBagConstraints contr1 = new GridBagConstraints();
+
+//Crée les contraintes de positionnement du label vide2e
+
+//coordonnée x dans le quadrillage
+contr1.gridx = 2;
+//coordonnée y dans le quadrillage
+contr1.gridy = 0;
+//hauteur standard
+contr1.gridheight = 1;
+//largeur standard
+contr1.gridwidth = 1;
+//remplit tout l'espace dans la cellule
+contr1.fill = GridBagConstraints.BOTH;
+//proportion d'espace horizontal occupée par ce composant
+contr1.weightx = 1;
+//proportion d'espace vertical occupée par ce composant
+contr1.weighty = 0.1;
+//position du composant dans la cellule
+contr1.anchor = GridBagConstraints.CENTER;
+
+/**
+* Crée le label contenant le label vide2
+*/
+JLabel vide2= new JLabel();
+
+//Affecte les contraintes au label vide2
+disposition1.setConstraints(vide2, contr1);
+
+//Ajoute le label vide2 au panneau
+panneau.add(vide2);
+
+
+// Crée une instance de GridBagConstraints pour le positionnement du message de bienvenue
+GridBagConstraints contr2 = new GridBagConstraints();
 
 // Crée les contraintes de positionnement du message de bienvenue
 
 // coordonnée x dans le quadrillage
-contr1.gridx = 1;
+contr2.gridx = 1;
 // coordonnée y dans le quadrillage
-contr1.gridy = 0;
-// cette cellule a la même hauteur que les autres
-contr1.gridheight = 1;
-// cette cellule est 3 fois plus large que les autres
-contr1.gridwidth = 3;
+contr2.gridy = 0;
+// hauteur standard
+contr2.gridheight = 1;
+// largeur standard
+contr2.gridwidth = 1;
 // remplit tout l'espace dans la cellule
-contr1.fill = GridBagConstraints.BOTH;
+contr2.fill = GridBagConstraints.BOTH;
 // proportion d'espace horizontal occupée par ce composant
-contr1.weightx = 1.0;
+contr2.weightx = 1;
 // proportion d'espace vertical occupée par ce composant
-contr1.weighty = 0.1;
+contr2.weighty = 0.1;
 // position du composant dans la cellule
-contr1.anchor = GridBagConstraints.NORTH;
+contr2.anchor = GridBagConstraints.CENTER;
 
 /**
-* Crée le label contenant le message de bienvenue
+* Crée le label contenant le message de bienvenue qui est centré sur la JLabel
 */
-JLabel messageBienvenue= new JLabel();
-messageBienvenue.setText("Bienvenue sur la page d'accueil du programme de dessin de véhicule");
+JLabel messageBienvenue= new JLabel("Bienvenue sur la page d'accueil du programme de dessin de véhicule",SwingConstants.CENTER);
+messageBienvenue.setOpaque(true);
+messageBienvenue.setBackground(Color.white);
+messageBienvenue.setForeground(Color.black);
 
 // Affecte les contraintes au message de bienvenue
-disposition1.setConstraints(messageBienvenue, contr1);
+disposition1.setConstraints(messageBienvenue, contr2);
 
-// Ajoute le message de bienvenue à la fenêtre
+// Ajoute le message de bienvenue au panneau
 panneau.add(messageBienvenue);
 
 /**
@@ -90,24 +156,24 @@ panneau.add(messageBienvenue);
 */
 
 // Crée une instance de GridBagConstraints pour le positionnement de la JComboBox
-GridBagConstraints contr2 = new GridBagConstraints();
+GridBagConstraints contr3 = new GridBagConstraints();
 
 // Crée les contraintes de positionnement de la JComboBox
 
 // coordonnée x dans le quadrillage
-contr2.gridx = 0;
+contr3.gridx = 0;
 // coordonnée y dans le quadrillage
-contr2.gridy = 1;
+contr3.gridy = 1;
 // hauteur standard
-contr2.gridheight = 1;
+contr3.gridheight = 1;
 //cette cellule est 2 fois plus large que les autres
-contr2.gridwidth = 2;
+contr3.gridwidth = 2;
 // proportion d'espace horizontal occupée par ce composant
-contr2.weightx = 1;
+contr3.weightx = 1;
 // proportion d'espace vertical occupée par ce composant
-contr2.weighty = 1;
+contr3.weighty = 1;
 // position du composant dans la cellule
-contr2.anchor = GridBagConstraints.WEST;
+contr3.anchor = GridBagConstraints.WEST;
 
 /**
 * JComboBox permettant de choisir le type de véhicule
@@ -116,8 +182,8 @@ String[] choix = { "voiture", "camion", "motocyclette"};
 JComboBox choixVehicule= new JComboBox(choix);
 
 // Affecte les contraintes de positionnement à la JComboBox
-disposition1.setConstraints(choixVehicule, contr2);
-// Ajoute la JComboBox à la fenêtre
+disposition1.setConstraints(choixVehicule, contr3);
+// Ajoute la JComboBox au panneau
 panneau.add(choixVehicule);
 
 /**
@@ -126,32 +192,32 @@ panneau.add(choixVehicule);
 */
 
 // Crée une instance de GridBagConstraints pour le positionnement du bouton "charger"
-GridBagConstraints contr3 = new GridBagConstraints();
+GridBagConstraints contr4 = new GridBagConstraints();
 
 // Crée les contraintes de positionnement du bouton "charger"
 
 // coordonnée x dans le quadrillage
-contr3.gridx = 2;
+contr4.gridx = 2;
 // coordonnée y dans le quadrillage
-contr3.gridy = 1;
+contr4.gridy = 1;
 // hauteur standard
-contr3.gridheight = 1;
+contr4.gridheight = 1;
 //largeur standard
-contr3.gridwidth = 1;
+contr4.gridwidth = 1;
 // proportion d'espace horizontal occupée par ce composant
-contr3.weightx = 1;
+contr4.weightx = 1;
 // proportion d'espace vertical occupée par ce composant
-contr3.weighty = 1;
+contr4.weighty = 1;
 // position du composant dans la cellule
-contr3.anchor = GridBagConstraints.EAST;
+contr4.anchor = GridBagConstraints.EAST;
 
 /**
 * Bouton permettant de charger un fichier existant
 */
 JButton charger= new JButton("charger");
 // Affecte les contraintes à ce champ
-disposition1.setConstraints(charger, contr3);
-// Ajoute le champ à la fenêtre
+disposition1.setConstraints(charger, contr4);
+// Ajoute le bouton "charger" au panneau
 panneau.add(charger);
 
 
@@ -162,30 +228,32 @@ panneau.add(charger);
 */
 
 // Crée une instance de GridBagConstraints
-GridBagConstraints contr4 = new GridBagConstraints();
-// Affecte les contraintes du champ Affichage
+GridBagConstraints contr5 = new GridBagConstraints();
+
+// Crée les contraintes du butons "valider"
 // coordonnée x dans le quadrillage
-contr4.gridx = 1;
+contr5.gridx = 1;
 // coordonnée y dans le quadrillage
-contr4.gridy = 2;
+contr5.gridy = 2;
 // hauteur standard
-contr4.gridheight = 1;
+contr5.gridheight = 1;
 // cette cellule est 3 fois plus large que les autres
-contr4.gridwidth = 3;
+contr5.gridwidth = 3;
 // proportion d'espace horizontal occupée par ce composant
-contr4.weightx = 1.0;
+contr5.weightx = 1.0;
 // proportion d'espace vertical occupée par ce composant
-contr4.weighty = 1.0;
+contr5.weighty = 1.0;
 // position du composant dans la cellule
-contr4.anchor = GridBagConstraints.CENTER;	
+contr5.anchor = GridBagConstraints.CENTER;	
 
 /**
 * création du bouton "valider"
 */
 JButton valider= new JButton("Valider");
-// Affecte les contraintes à ce champ
-disposition1.setConstraints(valider, contr4);
-// Ajoute le champ à la fenêtre
+
+// Affecte les contraintes au bouton "valider"
+disposition1.setConstraints(valider, contr5);
+// Ajoute le bouton "valider" au panneau
 panneau.add(valider);
 
 /**
@@ -196,7 +264,30 @@ JFrame frame = new JFrame("page d'accueil");
 // disposition2 permet à l'image de se retailler dynamiquement lorsque la taille de la fenêtre est changée par l'utilisateur
 GridBagLayout disposition2 = new GridBagLayout();
 frame.setLayout(disposition2);
-disposition2.setConstraints(panneau, contr1);
+//Crée une instance de GridBagConstraints pour le positionnement du message de bienvenue
+GridBagConstraints contr6 = new GridBagConstraints();
+
+//Crée les contraintes de positionnement du panneau
+
+//coordonnée x dans le quadrillage
+contr6.gridx = 1;
+//coordonnée y dans le quadrillage
+contr6.gridy = 0;
+//hauteur standard
+contr6.gridheight = 1;
+//largeur standard
+contr6.gridwidth = 3;
+//remplit tout l'espace dans la cellule
+contr6.fill = GridBagConstraints.BOTH;
+//proportion d'espace horizontal occupée par ce composant
+contr6.weightx = 1;
+//proportion d'espace vertical occupée par ce composant
+contr6.weighty = 0.1;
+//position du composant dans la cellule
+contr6.anchor = GridBagConstraints.CENTER;
+
+//affecte les contraintes de positionnement au panneau
+disposition2.setConstraints(panneau,contr6);
 
 // ajoute le panneau à la fenêtre
 frame.add(panneau);
